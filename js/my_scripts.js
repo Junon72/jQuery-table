e155610576cc56f7fb578e395e98211e2a428e86
Jussi Nousiainen <juno.athome@gmail.com>
2019-10-11 11:00:41 +0200

    Completed the challenge

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function()    {
-    $('h2').addClass('.underline'); //underline all the <h2> elements - with class .underline
-    $('ul').addClass('.border');
+    //Testing lines - Not relevant for the challenge
+    //$('h2').addClass('.underline'); //underline all the <h2> elements - with class .underline
+   //$('ul').addClass('.border');
+
+   //Challenge solution - Added new classes to js, to change the styling by addressin class instead of css, for not to make permanent changes
+   $('tr:odd').addClass('oddRow');
+   $('tr:even').addClass('evenRow');
 });
+
 
